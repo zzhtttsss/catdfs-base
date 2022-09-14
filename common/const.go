223ffb3fc59e13d6439f6a3da8a6b7cd37c97347
zzhtttsss <634879238@qq.com>
2022-09-14 20:27:38 +0800

    feat(docker): adapt docker and test add operation

--- common/const.go
+++ common/const.go
@@ -80,8 +80,9 @@ const (
 
 // const number
 const (
-	KB        = 1024
-	MB        = 1024 * KB
-	GB        = 1024 * MB
-	ChunkSize = 64 * MB
+	KB         = 1024
+	MB         = 1024 * KB
+	GB         = 1024 * MB
+	ChunkMBNum = 64
+	ChunkSize  = ChunkMBNum * MB
 )
