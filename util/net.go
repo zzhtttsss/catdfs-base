dc117031f7580502f572f8e6e065a0be16f61ea7
zzhtttsss <634879238@qq.com>
2022-09-26 16:44:54 +0800

    feat(raft): implement raft interface

---
+++ util/net.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"fmt"
+	"net"
+)
+
+func GetLocalIP() (ip string, err error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+	for _, addr := range addrs {
+		ipAddr, ok := addr.(*net.IPNet)
+		if !ok {
+			continue
+		}
+		if ipAddr.IP.IsLoopback() {
+			continue
+		}
+		if !ipAddr.IP.IsGlobalUnicast() {
+			continue
+		}
+		return ipAddr.IP.String(), nil
+	}
+	return "", fmt.Errorf("fail to find ip")
+}
