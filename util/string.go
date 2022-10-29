026af699741bc41fdc838e3b8c42ffc7259d4924
DividedMoon <yidian20000105@163.com>
2022-10-29 19:11:38 +0800

    feat(expansion):finish expand operation

--- util/string.go
+++ util/string.go
@@ -1,27 +1,16 @@
 package util
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/google/uuid"
-	"tinydfs-base/common"
 )
 
 func GenerateUUIDString() string {
 	return uuid.NewString()
 }
 
-func Marshal(a any) string {
-	bytes, err := json.Marshal(a)
-	if err != nil {
-		fmt.Printf("%v marshal failed.Error code %v.Error detail %s\n", a, common.JsonMarshalFailed, err)
+func Interfaces2TypeArr[T interface{}](arr []interface{}) (res []T) {
+	for _, a := range arr {
+		res = append(res, a.(T))
 	}
-	return string(bytes)
-}
-
-func Unmarshal(s string, a any) {
-	err := json.Unmarshal([]byte(s), a)
-	if err != nil {
-		fmt.Printf("%s unmarshal failed.Error code %v.Error detail %s\n", s, common.JsonUnmarshalFailed, err)
-	}
+	return
 }
